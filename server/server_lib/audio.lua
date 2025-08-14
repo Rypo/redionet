fbe9d6778d7708febe6e3630abb562028c85c20b
Rypo <ryan@gust.dev>
2025-08-14 10:24:36 -0500

    fix: clients no longer drift out of sync under normal operating conditions
    The server now waits to receive the expected number of replies based on host lookup.
    
    Known conditions where client audio can still desynchronize:
    - chunk unloads
    - peripheral speaker/modem detached
    - client reboots

--- server/server_lib/audio.lua
+++ server/server_lib/audio.lua
@@ -145,6 +145,7 @@ function Buffer.new(handle, song_id)
         self.done_write = true
         self.song_id = "NULL" -- avoid setting nil because of the nil == nil behavior
         self.buffer = nil
+        return nil
     end
 
     function self:stream_complete()
@@ -219,29 +220,52 @@ end
 local function transmit_audio(data_buffer)
     local buffer = data_buffer:next()
     local sub_state = {
-        active_stream_id=STATE.data.active_stream_id,
+        active_stream_id = STATE.data.active_stream_id,
         song_id = data_buffer.song_id, -- add in local song_id for interrupts
         chunk_id = data_buffer.total_write.chunks
     }
 
     -- debug.debug()
-    local receivers = { rednet.lookup("PROTO_AUDIO") } -- TODO: expensive?
+    local receivers = { rednet.lookup("PROTO_AUDIO") }
+    -- TODO: expensive, can/should async? 
+    -- OR better yet, maintain a list of active clients based on # replies in previous iters. 3 consec timeout = client dead
+    local n_receivers = #receivers
     rednet.broadcast({ "PLAY", {buffer, sub_state} }, 'PROTO_AUDIO')
-
+    -- local send_tasks = {}
+    -- for i, recv_id in ipairs(receivers) do
+    --     send_tasks[i] = function () rednet.send(recv_id, { "PLAY", {buffer, sub_state} }, 'PROTO_AUDIO') end
+    -- end
+    -- pcall(parallel.waitForAll, table.unpack(send_tasks)) -- TODO: prefer over broadcast ?
     
     local play_tasks = {}
-    local timeout = AUDIO_CHUNK_DURATION -- + 0.15 -- 3 extra ticks 
+    local timeout = AUDIO_CHUNK_DURATION + 0.15 -- 3 extra ticks / 150ms 
 
+    local replies = {}
+    
     for i, recv_id in ipairs(receivers) do
-        -- rednet.send(recv_id, { "PLAY", {buffer, sub_state} }, 'PROTO_AUDIO') -- TODO: prefer over broadcast ?
-        play_tasks[i] = function() rednet.receive("PROTO_AUDIO_NEXT", timeout) end
+        play_tasks[i] = function()
+            while #replies < n_receivers do 
+                local id,msg = rednet.receive("PROTO_AUDIO_NEXT", timeout)
+                if id then -- weak check. Doesn't care who replied, only number received
+                    local timestamp_ms = os.epoch("local")
+                    table.insert(replies, timestamp_ms)
+                    
+                    -- os.queueEvent("log_message", string.format('(%s) %d %s | n=%d/%d', ("%0.3f"):format(timestamp_ms/1000):sub(6), id, msg, #replies, #receivers), "DEBUG")
+                    chat.log_message(string.format('(%s) %d %s | n=%d/%d', ("%0.3f"):format(timestamp_ms/1000):sub(6), id, msg, #replies, n_receivers), "DEBUG")
+                else
+                    n_receivers = n_receivers - 1 -- assume connection lost on timeout; lookup too disruptive
+                end
+            end
+        end
     end
-
+    
     local prefill_buffer = function () data_buffer:read_n(2) end
     -- THIS is where we can sneak in pre-populate -- while waiting on speakers. 
     -- as long as prepop takes < ~2.75 seconds, it should never cause any delay 
     local ok, err = pcall(parallel.waitForAll, table.unpack(play_tasks), prefill_buffer)
 
+    local desync_ms = (math.max(table.unpack(replies)) - math.min(table.unpack(replies)))
+    chat.log_message(string.format('client desync: %dms | n=%d/%d', desync_ms, #replies, #receivers), "INFO")
 
     if not ok then
         STATE.data.error_status = "PLAYBACK_ERROR" -- redundant, now set in playback_stopped
@@ -264,10 +288,7 @@ local function process_audio_data(data_buffer)
         parallel.waitForAny(
         -- function () play_audio_chunk(data_buffer:next(), data_buffer.song_id) end,
             function() os.pullEvent("request_next_chunk") end,
-            function()
-                os.pullEvent("playback_stopped")
-                data_buffer:destroy()
-            end
+            function() os.pullEvent("playback_stopped") end
         )
         if STATE.data.status<1 or STATE.data.active_stream_id==nil then break end
     end
@@ -288,6 +309,8 @@ local function set_state_queue_empty()
     STATE.data.active_stream_id = nil
 end
 
+---Moves the queue forward 1 song. Accounts for loop_mode state.   
+---@return table? song_meta_data meta data of next queued song or nil if queue empty  
 local function advance_queue()
     if STATE.data.loop_mode > 0 and STATE.data.active_song_meta then
         if STATE.data.loop_mode == 1 then     -- Loop Queue
@@ -310,19 +333,18 @@ local function advance_queue()
 end
 
 function M.play_song(song_meta)
-    if song_meta then
-        if STATE.data.active_song_meta ~= song_meta then
-            if STATE.data.active_stream_id ~= nil then -- if song currently streaming, stop  
-                M.stop_song()
-            end
+    local state = STATE.to_string()
+    local smeta = song_meta and STATE.to_string(song_meta)
+    if song_meta and song_meta.id then
+        if STATE.data.active_stream_id and STATE.data.active_stream_id ~= song_meta.id then
+            debug.debug()
+            M.stop_song() -- if different song currently streaming, stop
         end
-        STATE.data.active_song_meta = song_meta
-        STATE.data.active_stream_id = nil
-    
-    -- elseif STATE.data.active_song_meta == nil then
-    --     STATE.data.active_song_meta = advance_queue() -- Now done in the audio event loop
+            
+        STATE.data.active_song_meta = song_meta -- overwrite current meta (may be identical)
     end
-
+    
+    debug.debug()
     STATE.data.status = 1      -- needs to be at end to overwrite stop_song()
     -- STATE.data.active_stream_id = nil -- whenever this is nil, we will trigger a download on fetch_audio
     -- os.queueEvent("redraw_screen", "audio.play_song")
@@ -336,19 +358,20 @@ function M.stop_song()
     --     speaker.stop()
     --     os.queueEvent("playback_stopped")
     -- end
-    os.queueEvent("playback_stopped") -- pulled by buffer / process_audio_data
+    os.queueEvent("playback_stopped") -- pulled by process_audio_data
     STATE.data.active_stream_id = nil
     STATE.data.status = 0
-    os.queueEvent("redraw_screen", "audio.stop_song")
+    -- os.queueEvent("redraw_screen", "audio.stop_song")
 end
 
 function M.skip_song()
+    -- cannot rely on nil/fetch_audio behaviour because of looping
     local up_next_meta = advance_queue()
     M.play_song(up_next_meta)
 end
 
 function M.toggle_play_pause()
-    local state_begin = STATE.format_state()
+    local state_begin = STATE.to_string()
 
     -- if STATE.data.is_paused or STATE.data.is_paused == nil then -- first click nil
     if STATE.data.status < 1 then
@@ -357,7 +380,7 @@ function M.toggle_play_pause()
         M.stop_song()
     end
 
-    local state_end = STATE.format_state()
+    local state_end = STATE.to_string()
 
     -- debug.debug()
 end
@@ -369,6 +392,7 @@ function M.audio_loop()
         ["audio_chunk_ready"] = true,
         ["playback_stopped"] = true,
     }
+    local dbuffer -- data buffer, need out of loop body to properly destroy in the event of early termination
     while true do
         local eventData = { os.pullEvent() }
         local event = eventData[1]
@@ -388,17 +412,17 @@ function M.audio_loop()
             end
 
 
-            local state              = STATE.format_state()
+            local state              = STATE.to_string()
 
-            -- debug.debug()
+            debug.debug()
             
-            STATE.send_state() -- may trigger more than strictly necessary, but centeralizing eliminates need for a patchwork of calls elsewhere
+            STATE.broadcast("audio_loop - ".. event) -- may trigger more than strictly necessary, but centeralizing eliminates need for a patchwork of calls elsewhere
 
             if event == "fetch_audio" then
                 local has_data_stream    = (STATE.data.active_stream_id ~= nil)
                 local has_correct_stream = has_data_stream and (STATE.data.active_stream_id == STATE.data.active_song_meta.id)
                 
-                -- debug.debug()
+                debug.debug()
                 -- This will always execute if queued properly and should_play==true, but keep as safety check to avoid re-downloading an actively streaming song
                 if should_play and not has_correct_stream then
                     network.download_song(STATE.data.active_song_meta.id)
@@ -407,20 +431,25 @@ function M.audio_loop()
             elseif event == "audio_chunk_ready" then
                 
                 local handle = STATE.data.response_handle
+                if not handle then error('bad state: read handle is nil', 0) end -- appease the linter (state should be unreachable)
                 local h_pos = handle.seek()
 
-                -- debug.debug()
+                debug.debug()
 
                 if should_play then
                     -- makes more sense to announce here since this is the last moment before audio actually plays
                     chat.announce_song(STATE.data.active_song_meta.artist, STATE.data.active_song_meta.name)
-                    
-                    local dbuffer = Buffer.new(handle, STATE.data.active_song_meta.id)
+                    if dbuffer then
+                        dbuffer = dbuffer:destroy() -- if it still exists, the song didn't complete. cannot guarantee clean state
+                    end
+                    dbuffer = Buffer.new(handle, STATE.data.active_song_meta.id)
 
                     -- debug.debug()
                     local song_completed = process_audio_data(dbuffer)
                     if song_completed then
-                        STATE.data.active_song_meta = advance_queue() -- EDGE CASE?: click skip song just as a song ends => skips over a song
+                        -- EDGE CASE?: click skip song just as a song ends => skips over a song
+                        STATE.data.active_song_meta = advance_queue() -- can't set active_song_meta = nil in case of looping
+                        dbuffer = nil -- if completed, don't need to destroy, file handle will have already been closed
                     end
                     STATE.data.active_stream_id = nil -- (re)download on next play, regardless of if finished
                     -- debug.debug()
