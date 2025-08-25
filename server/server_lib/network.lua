a613f64125cffab2a0859c793b3efee25e6411a9
Rypo <ryan@gust.dev>
2025-08-25 10:05:40 -0500

    fix: set max download attempts to prevent infinitely retrying downloads

--- server/server_lib/network.lua
+++ server/server_lib/network.lua
@@ -9,9 +9,11 @@ local M = {}
 
 M.config = {
     api_base_url = "https://ipod-2to6magyna-uc.a.run.app/",
-    version = "2.1"
+    version = "2.1",
+    max_dl_attempts = 3,
 }
 
+local dl_attempt = 0
 
 function M.format_download_url(song_id)
     if not song_id then return nil end
@@ -19,6 +21,7 @@ function M.format_download_url(song_id)
 end
 
 function M.download_song(song_id)
+    if STATE.data.last_download_id ~= song_id then dl_attempt = 0 end -- reset count if song different
     STATE.data.last_download_id = song_id
     STATE.data.is_loading = true
     http.request({ url = M.format_download_url(song_id), binary = true })
@@ -40,6 +43,7 @@ function M.handle_http_download()
                     local handle = eventData[3]
                     STATE.data.is_loading = false
                     STATE.data.error_status = false
+                    dl_attempt = 0 -- reset here as well in case looping
                     
                     if STATE.data.response_handle then
                         pcall(STATE.data.response_handle.close) -- Buffer should have closed already, but once more for good measure
@@ -55,11 +59,22 @@ function M.handle_http_download()
                     local err = eventData[3]
                     STATE.data.is_loading = false
                     STATE.data.error_status = "DOWNLOAD_ERROR"
+                    dl_attempt = dl_attempt + 1
                     
                     STATE.data.active_stream_id = nil
+                    if dl_attempt < M.config.max_dl_attempts then
+                        os.queueEvent("fetch_audio")
+                    else
+                        err = "Download Retry Limit"
+                        STATE.data.status = 0
+                        os.queueEvent("playback_stopped", STATE.data.error_status)
+                    end
 
-                    os.queueEvent("fetch_audio")
-                    chat.log_message(STATE.data.error_status .. ": " .. tostring(err), "ERROR")
+                    chat.log_message(("%s: %s | Attempt: %d/%d"):format(STATE.data.error_status, err, dl_attempt, M.config.max_dl_attempts), "ERROR")
+                    
+                    if dl_attempt == M.config.max_dl_attempts then
+                        dl_attempt = 0 -- allow to go another round of max_attempts if queue up identical song or play->stop->play
+                    end
                 end
             end
         end
